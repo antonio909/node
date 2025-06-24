b0ec75163f1aad3e5ae285a64079887ca17c6fb9
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-06-24 19:54:40 -0300

    Create vcbuild.bat

---
+++ vcbuild.bat
@@ -0,0 +1,100 @@
+@if not defined DEBUG_HELPER @ECHO OFF
+
+:: Other scripts rely on the environment variables set in this script, so we
+:: explicitly allow them to persist in the calling shell.
+endlocal
+
+set "arg=%1"
+if /i "%arg:~-1%"=="?" goto help
+if /i "%arg:~-4%"=="help" goto help
+
+cd %~dp0
+
+set JS_SUITES=default
+set NATIVE_SUITES=addons js-native-api node-api
+@rem CI_* variables should be kept synchronized with the ones in Mikefile
+set "CI_NATIVE_SUITES=%NATIVE_SUITES% benchmark"
+set "CI_JS_SUITES=%JS_SUITES% pummel"
+set CI_DOC=doctool
+@rem Same as the test-ci target in Makefile
+set "common_test_suites=%JS_SUITES% %NATIVE_SUITES%&set build_addons=1&set build_js_native_api_tests=1&set build_node_api_tests=1"
+
+@rem Process arguments.
+set config=Release
+set target=Build
+set target_arch=x64
+set ltcg=
+set target_env=
+set noprojgen=
+set projgen=
+set clang_cl=
+set ccache_path=
+set nobuild=
+set sign=
+set nosnapshot=
+set nonpm=
+set cctest_args=
+set test_args=
+set stage_package=
+set package=
+set msi=
+set upload=
+set licensertf=
+set lint_js=
+set lint_js_build=
+set lint_js_fix=
+set lint_cpp=
+set lint_md=
+set lint_md_build=
+set format_md=
+set i18n_arg=
+set download_arg=
+set build_release=
+set configure_flags=
+set enable_vtune_arg=
+set build_addons=
+set dll=
+set enable_static=
+set build_js_native_api_tests=
+set build_node_api_tests=
+set test_node_inspect=
+set test_check_deopts=
+set v8_test_options=
+set v8_build_options=
+set http2_debug=
+set nhttp2_debug=
+set link_module=
+set no_cctest=
+set cctest=
+set openssl_no_asm=
+set no_shared_rohead=
+set doc=
+set extra_msbuild_args=
+set compile_commands=
+set cfg=
+set exit_code=0
+
+:next-arg
+if "%1"=="" goto args-done
+if /i "%1"=="debug"          set config=Debug&goto arg-ok
+if /i "%1"=="release"        set config=Release&set ltcg=1&set cctest=1&goto arg-ok
+if /i "%1"=="clean"          set target=Clean&goto arg-ok
+if /i "%1"=="testclean"      set target=TestClean&goto arg-ok
+if /i "%1"=="ia32"           set target_arch=x86&goto arg-ok
+if /i "%1"=="x86"            set target_arch=x86&goto arg-ok
+if /i "%1"=="x64"            set target_arch=x64&goto arg-ok
+if /i "%1"=="arm64"          set target_arch=arm64&goto arg-ok
+if /i "%1"=="vs2022"         set target_env=vs2022&goto arg-ok
+if /i "%1"=="noprojgen"      set noprojgen=1&goto arg-ok
+if /i "%1"=="projgen"        set projgen=1&goto arg-ok
+if /i "%1"=="clang-cl"       set clang_cl=1&goto arg-ok
+if /i "%1"=="ccache"         set "ccache_path=%2%"&goto arg-ok
+if /i "%1"=="nobuild"        set nobuild=1&goto arg-ok
+if /i "%1"=="nosign"         set "sign="&echo Note: vcbuild no longer signs by default. "nosign" is redundant.&goto arg-ok
+if /i "%1"=="sign"           set sign=1&goto arg-ok
+if /i "%1"=="nosnapchot"     set nosnapchot=1&goto arg-ok
+if /i "%1"=="nonpm"          set nonpm=1&goto arg-ok
+if /i "%1"=="ltcg"           set ltcg=1&goto arg-ok
+if /i "%1"=="licensertf"     set licensertf=1&goto arg-ok
+if /i "%1"=="test"           set test_args=%test_args% %common_test_suites%&set lint_cpp=1&set lint_js=1&set lint_md=1&goto arg-ok
+if /i "%1"=="test-ci-native" set test_args=%test_args% %test_ci_args% -p tap --logfile test.tap %CI_NATIVE_SUITES% %CI_DOC%&set build_addons=1&set build_js_native_api_tests=1&set build_node_api_tests=1&set
